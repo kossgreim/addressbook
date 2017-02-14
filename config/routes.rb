09d986f582d20ca332774e540c31c5bd126151a4
swtnext <swtnext@gmail.com>
2017-02-14 21:45:45 +0100

    setup api versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+
+  # API v1 routes
+  constraints subdomain: 'api' do
+    scope module: 'api' do
+      namespace :v1 do
+
+      end
+    end
+  end
 end
