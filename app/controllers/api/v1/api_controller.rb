c69330c962d2b9a7a41d91a770602b51b9d7766b
swtnext <swtnext@gmail.com>
2017-02-15 22:34:29 +0100

    api and organizations controllers

---
+++ app/controllers/api/v1/api_controller.rb
@@ -0,0 +1,19 @@
+module Api::V1
+  class ApiController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+
+    private
+    def record_not_found(error)
+      render json: { errors: error.message }, status: :not_found
+    end
+
+    private
+
+    def render_error(resource, status)
+      render json: resource,
+             status: status,
+             adapter: :json_api,
+             serializer: ActiveModel::Serializer::ErrorSerializer
+    end
+  end
+end
---
+++ app/controllers/api/v1/organizations_controller.rb
@@ -0,0 +1,50 @@
+module Api::V1
+  class OrganizationsController < ApiController
+
+    before_action :set_organization, only: [:show, :update, :destroy]
+    include PaginationParams
+
+    def index
+      render json: Organization.paginate(pagination_params)
+    end
+
+    def show
+      organization = Organization.find(params[:id])
+
+      render json: organization
+    end
+
+    def create
+      organization = Organization.new(organization_params)
+
+      if organization.save
+        render json: organization, status: :created
+      else
+        render_error(organization, :unprocessable_entity)
+      end
+    end
+
+    def update
+      if @organization.update_attributes(organization_params)
+        render json: @organization, status: :ok
+      else
+        render_error(@organization, :unprocessable_entity)
+      end
+    end
+
+    def destroy
+      @organization.destroy
+      head 204
+    end
+
+    private
+
+    def set_organization
+      @organization = Organization.find(params[:id])
+    end
+
+    def organization_params
+      ActiveModelSerializers::Deserialization.jsonapi_parse(params)
+    end
+  end
+end
