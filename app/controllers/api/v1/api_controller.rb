b8b71e3ab8985e10b871e225f303713f853fa695
swtnext <swtnext@gmail.com>
2017-02-15 22:51:02 +0100

    style fix

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -3,6 +3,7 @@ module Api::V1
     rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
 
     private
+
     def record_not_found(error)
       render json: { errors: error.message }, status: :not_found
     end
