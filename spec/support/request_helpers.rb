9d5aebc0e8247185686f61bcbde8b11fd30d6a1a
swtnext <swtnext@gmail.com>
2017-02-16 19:29:02 +0100

    add request helpers

--- spec/support/request_helpers.rb
+++ spec/support/request_helpers.rb
@@ -1,7 +1,20 @@
 module Requests
+
   module JsonHelpers
     def json
       JSON.parse(response.body)
+    end
+  end
+
+  module JsonAPI
+    def create_request(type, attributes)
+      {
+          data: { type: type, attributes: attributes }
+      }
+    end
+
+    def response_attribute(name)
+      json['data']['attributes'][name]
     end
   end
 end
