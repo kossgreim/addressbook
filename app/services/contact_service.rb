d134dc2fad4e033d1e3f674270e6bce387944101
swtnext <swtnext@gmail.com>
2017-02-18 20:14:45 +0100

    add comments and improvements to ContactService

--- app/services/contact_service.rb
+++ app/services/contact_service.rb
@@ -1,15 +1,19 @@
 class ContactService
 
   def initialize(args={})
+    # initializes Firebase database's url
     base_uri = args[:base_uri] || ENV['FIREBASE_DATABASE_URL']
+    # setting the database's secret key
     secret_key = args[:secret_key] || ENV['FIREBASE_API_KEY']
+    # collection name in the database
     @path = args[:collection_name] || 'contacts'
 
     @client = Firebase::Client.new(base_uri, secret_key)
   end
 
-  def create(contact)
-    path = get_path(contact.organization_id)
+  # creates a new record
+  def create(contact, organization_id)
+    path = get_path(organization_id)
     result = handle_response(@client.push(path, contact.attributes))
     return if result.blank?
 
@@ -30,18 +34,23 @@ class ContactService
     result.success?
   end
 
+  # finds record by it's id
   def find(organization_id, id)
     path = get_path(organization_id, id)
     contact = handle_response(@client.get(path))
-    raise ActiveRecord::RecordNotFound unless contact.present?
+    raise ActiveRecord::RecordNotFound.new("Couldn't find Contact with id=#{id}") unless contact.present?
 
     set_contact_data(contact, id)
   end
 
+  # finds all records for an organization
   def find_by_organization(organization_id, args={})
     path = get_path(organization_id)
     contacts = handle_response(@client.get(path, args))
     arr = []
+    # we return an empty array if we didn't get any contacts
+    return arr unless contacts.present?
+    # wrapping data data with Contact model
     contacts.each do |id, contact|
       arr << set_contact_data(contact, id)
     end
@@ -51,16 +60,19 @@ class ContactService
 
   private
 
+  # handles firebase database's response
   def handle_response(response)
     return false unless response.success?
 
     response.body
   end
 
+  # wraps data with Contact model
   def set_contact_data(attributes, id)
     Contact.new(attributes.merge({ id: id }))
   end
 
+  # generates path for Firebase database's records
   def get_path(organization_id, id = nil)
     path = "#{@path}/organization_#{organization_id}"
     path << "/#{id}" if id
