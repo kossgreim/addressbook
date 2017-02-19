1b186139de840d8ce0a8a9bee2bdf9dadc772796
swtnext <swtnext@gmail.com>
2017-02-19 12:03:21 +0100

    add setup db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Creating admin and organization
+org = Organization.new(name: 'First Organization Test')
+org.save!
+puts "Organization #{org.name} has been created"
+
+admin = User.new(
+    first_name: 'Admin',
+    last_name: 'Admin',
+    email: 'admin@example.com',
+    password: '12345678',
+    admin: true,
+    organization_id: org.id
+)
+admin.save!
+
+puts 'Test admin account has been created'
+puts '-'*10
+puts 'Your login: admin@example.com'
+puts 'Your password: 12345678'
+
